e4351899de22aa503933be614711dcddf0a24850
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-11 21:19:28 +0000

    ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter name for new store"
+print "> "
+store_name = $stdin.gets.chomp
+puts "Enter annual revenue for new store"
+print "> "
+ann_rev = $stdin.gets.chomp
+
+
+new_store = Store.new(name: store_name, annual_revenue: ann_rev)
+
+
+new_store.valid?
+new_store.errors.each{|key, value| puts "#{key} #{value}"}
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 2}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :must_have_men_or_women_apparel
 
+  def must_have_men_or_women_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "& womens_apparel are both false, what are you selling?!")
+    end
+  end
 end
